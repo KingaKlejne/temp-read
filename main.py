cef1b3eb407aec19bab9342250c6210ecb6bc9cc
Kinga Klejne <kingaklejne@gmail.com>
2022-10-06 21:08:41 +0200

    adding database connection

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 from flask import Flask, request, jsonify
 import re
 import statistics
+import sqlite3
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 
 data_list = []
 searched_data = []
 
 # TODO 1: Create Flask App
 app = Flask(__name__)
+
+# TODO 1A: Create Database
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///readings.db"
+# Optional: But it will silence the deprecation warning in the console.
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+# TODO 1B: Create Table
+class Readings(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.String(250), nullable=False)
+    room = db.Column(db.String(250), nullable=False)
+    location = db.Column(db.String(250), nullable=False)
+    reading = db.Column(db.Float, nullable=False)
+
+
+db.create_all()
 
 # TODO 2: ISO8601 formatting
 regex = r'^(-?(?:[1-9][0-9]*)?[0-9]{4})(1[0-2]|0[1-9])(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9])([0-5][0-9])' \
@@ -29,12 +49,16 @@ def convert_temp(temp):
 def post_route():
     try:
         data = request.get_json()
+        place = data['label'].split(",")
+        data["reading"] = convert_temp(data["reading"])
+        new_reading = Readings(time=data['time'], room=place[0], location=place[1], reading=data['reading'])
+        db.session.add(new_reading)
+        db.session.commit()
         if not isinstance(data["reading"], int) and not isinstance(data["time"], str) and not isinstance(data["label"],
                                                                                                          str):
             return {"error": "Value-Type not supported!"}, 400
         if not datetime_valid(data["time"]):
             return {"error": "Time must be in ISO8601 format!"}, 400
-        data["reading"] = convert_temp(data["reading"])
         data_list.append(data)
         return jsonify(data_list)
     except TypeError:
